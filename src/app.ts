5bb685a2d2631eae0a8b843eb4f671a353e56418
souzaWill <contato.willsouza@gmail.com>
2025-07-20 01:54:26 -0300

    config cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import router from './routes/index';
 import { exceptionHandler } from './middleware/exceptionHandler';
-import cors from 'cors'; //TODO set cors permissions
+import cors from 'cors';
+import { CORS_ORIGIN } from './config/env';
 
 const app = express();
 
-app.use(express.json()).use(cors()).use(router).use(exceptionHandler);
+app
+  .use(express.json())
+  .use(
+    cors({
+      origin: CORS_ORIGIN,
+      credentials: true,
+    }),
+  )
+  .use(router)
+  .use(exceptionHandler);
 
 export default app;
