b3c347eb5a111056f95973ad5cef7bd825c0ddab
souzaWill <contato.willsouza@gmail.com>
2025-07-20 00:36:17 -0300

    add cors permissions

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/index';
 import { exceptionHandler } from './middleware/exceptionHandler';
+import cors from 'cors';//TODO set cors permissions 
+
 
 const app = express();
 
-app.use(express.json()).use(router).use(exceptionHandler);
+app.use(express.json()).use(cors()).use(router).use(exceptionHandler);
 
 export default app;
