71131bfd6a891c39ed9ffa06f6a7349596160ea0
souzaWill <contato.willsouza@gmail.com>
2025-07-24 12:53:25 -0300

    feat: protect student routes

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { validateData } from '../middlewares/validationMiddleware';
+import { authMiddleware } from '../middlewares/authMiddleware';
 import { createStudentSchema, updateStudentSchema } from '../schemas/studentSchema';
 import { index, store, show, destroy, update } from '../controllers/student.controller';
 
 const router = Router();
 
+router.use(authMiddleware)
 router.get('/', index);
 router.post('/', validateData(createStudentSchema), store);
 router.put('/:id', validateData(updateStudentSchema), update);
